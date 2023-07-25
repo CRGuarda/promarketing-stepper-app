f8293a7e1861737e4628cee4938b17b53215c89a
CRGuarda <crguarda95@gmail.com>
2023-07-24 22:03:46 -0500

    some styling for Step component

--- src/components/Step.tsx
+++ src/components/Step.tsx
@@ -1,3 +1,10 @@
+import { SuccessIcon } from '@/components/Icons'
+
 export const Step = ({ label }: { label: string }) => {
-  return <div>{label}</div>
+  return (
+    <div className='flex flex-col items-center'>
+      <SuccessIcon />
+      {label}
+    </div>
+  )
 }
--- src/components/Stepper.tsx
+++ src/components/Stepper.tsx
@@ -4,5 +4,9 @@ export const Stepper = ({ children }: { children: React.ReactNode }) => {
   const childrens = Children.count(children)
   console.log(childrens)
 
-  return <div>{children}</div>
+  return (
+    <div className='w-full grid gap-10 grid-cols-[repeat(auto-fit,minmax(32px,1fr))] justify-items-center'>
+      {children}
+    </div>
+  )
 }
